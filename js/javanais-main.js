a6f4da36aed76670122018156526c898a819e7a5
Unknown <mickael.foucaud@gmail.com>
2017-07-01 15:31:26 +0200

    feat: ajout recursive

--- js/javanais-main.js
+++ js/javanais-main.js
@@ -1,3 +1,8 @@
+function isVoyelle(caractere){
+	var voyelles = "AÀÂEÉÈÊËIÎÏYOÔUÛÙaàâeéèêëiîïyoôuûù";
+	return voyelles.indexOf(caractere)!=-1
+}
+
 exports.francaisVersJavanais = function (texteATraduire){
   return francaisVersJavanaisRegex(texteATraduire);
 }
@@ -30,12 +35,79 @@ exports.javanaisVersFrancais = function (texteATraduire){
 
 
 /*
+* Fonction permettant la traduction francais->javanais  par recursivite
+*/
+ function francaisVersJavanaisRecursivite(texteATraduire, precedeParConsonne, index){
+   if(precedeParConsonne == undefined){precedeParConsonne = true;}
+ 	if(index == undefined){index = 0;}
+
+ 	var lettreEnCours = texteATraduire.charAt(index);
+ 	if(index<=texteATraduire.length-1){
+
+ 		if(isVoyelle(lettreEnCours)){
+ 			//voyelle precedée par consonne
+ 			if(precedeParConsonne){
+ 				texteATraduire = texteATraduire.slice(0,index)+'av'+texteATraduire.slice(index);
+ 				index+=2;
+ 			}
+ 			//console.log('tradRecursiveFrancaisEnJavanais ' +texteATraduire);
+ 			precedeParConsonne=false;
+ 		}else{
+ 			//consonne
+ 			//console.log('tradRecursiveFrancaisEnJavanais ' +texteATraduire);
+ 			precedeParConsonne=true;
+ 		}
+ 		index++
+ 		texteATraduire = francaisVersJavanaisRecursivite(texteATraduire, precedeParConsonne, index)
+ 	}
+
+ 	 return texteATraduire;
+}
+
+exports.francaisVersJavanaisRecursive = function (texteATraduire){
+  return francaisVersJavanaisRecursivite(texteATraduire);
+}
+
+/*
+* Fonction permettant la traduction  javanais->francais par regexp
+*/
+ function javanaisVersFrancaisRecursivite(texteATraduire, index){
+   if(index == undefined){index = 0;}
+ 	var textTemp = texteATraduire.toLowerCase();
+ 	var indexTrouve = textTemp.indexOf('av',index);
+
+ 	if(indexTrouve!=-1) {
+ 		if(indexTrouve>0){
+ 			lettrePrecedente=textTemp.charAt(indexTrouve-1);
+ 		}
+ 		lettreSuivante=textTemp.charAt(indexTrouve+2);
+ 		//console.log('tradRecursiveJavanaisVersFrancais : ' + lettrePrecedente+'av'+lettreSuivante);
+ 		if((indexTrouve==0 || !isVoyelle(lettrePrecedente)) && isVoyelle(lettreSuivante)){
+ 			//le av correspond à un av ajouté par l'argot
+ 			texteATraduire = texteATraduire.slice(0,indexTrouve)+texteATraduire.slice(indexTrouve+2);
+ 		}
+ 		 texteATraduire = javanaisVersFrancaisRecursivite(texteATraduire, index+2);
+ 	}
+ 	return texteATraduire;
+}
+
+exports.javanaisVersFrancaisRecursive = function (texteATraduire){
+  return javanaisVersFrancaisRecursivite(texteATraduire);
+}
+
+
+
+
+
+/*
 * Fonction lancant la trduction francais vers javanais
 */
 function traduireEnJavanais(methode) {
 	var txtATraduire = $('textarea#txtAreaFrancais_'+methode).val();
 	if(1==methode){
 		$('textarea#txtAreaJavanais_'+methode).val(francaisVersJavanaisRegex(txtATraduire));
+	} else if(2==methode){
+		$('textarea#txtAreaJavanais_'+methode).val(francaisVersJavanaisRecursivite(txtATraduire));
 	}
 }
 
@@ -48,7 +120,6 @@ function traduireEnFrancais(methode) {
 	if(1==methode){
 		$('textarea#txtAreaFrancais_'+methode).val(javanaisVersFrancaisRegex(txtATraduire));
 	}else if(2==methode){
-		$('textarea#txtAreaFrancais_'+methode).val(javanaisVersFrancaisRecursive(txtATraduire));
-
+		$('textarea#txtAreaFrancais_'+methode).val(javanaisVersFrancaisRecursivite(txtATraduire));
 	}
 }
--- test/javanais-main-test.js
+++ test/javanais-main-test.js
@@ -36,10 +36,38 @@ describe("Tradcuteur francais<->javanais", function(){
 		});
 	});
 
-	describe("Traducteur via recursive javanais->francais", function(){
+	describe("Traducteur via recursive", function(){
 
+		it("convertit de francais a javanais (recursive)", function() {
+			var jBonjour   = traducteur.francaisVersJavanaisRecursive("bonjour");
+			var jChante = traducteur.francaisVersJavanaisRecursive("chante");
+			var jMoyen  = traducteur.francaisVersJavanaisRecursive("moyen");
+			var jExemple  = traducteur.francaisVersJavanaisRecursive("exemple");
+			var jAu  = traducteur.francaisVersJavanaisRecursive("au");
+			var jLarch  = traducteur.francaisVersJavanaisRecursive("Et maintenant quelque chose de complètement différent, le mélèze");
 
+			expect(jBonjour).to.equal("bavonjavour");
+			expect(jChante).to.equal("chavantave");
+			expect(jMoyen).to.equal("mavoyen");
+			expect(jExemple).to.equal("avexavemplave");
+			expect(jAu).to.equal("avau");
+			expect(jLarch).to.equal("avEt mavaintavenavant qavuelqavue chavosave dave cavomplavètavemavent daviffavéravent, lave mavélavèzave");
+		});
 
-	})
+		it("convertit de javanais a francais (recursive)", function() {
+			var fBonjour   = traducteur.javanaisVersFrancaisRecursive("bavonjavour");
+			var fChante = traducteur.javanaisVersFrancaisRecursive("chavantave");
+			var fMoyen  = traducteur.javanaisVersFrancaisRecursive("mavoyen");
+			var fExemple  = traducteur.javanaisVersFrancaisRecursive("avexavemplave");
+			var fAu  = traducteur.javanaisVersFrancaisRecursive("avau");
+			var fLarch  = traducteur.javanaisVersFrancaisRecursive("avEt mavaintavenavant qavuelqavue chavosave dave cavomplavètavemavent daviffavéravent, lave mavélavèzave");
+			expect(fBonjour).to.equal("bonjour");
+			expect(fChante).to.equal("chante");
+			expect(fMoyen).to.equal("moyen");
+			expect(fExemple).to.equal("exemple");
+			expect(fLarch).to.equal("Et maintenant quelque chose de complètement différent, le mélèze");
+		});
+
+	});
 
 })
