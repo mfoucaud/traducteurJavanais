da730770ec13395221b796c1feb1ba0dfd53de32
Unknown <mickael.foucaud@gmail.com>
2017-07-01 14:00:39 +0200

    feat: trad via regexp

--- js/javanais-main.js
+++ js/javanais-main.js
@@ -1,7 +1,54 @@
 exports.francaisVersJavanais = function (texteATraduire){
-  return texteATraduire;
+  return francaisVersJavanaisRegex(texteATraduire);
+}
+
+/*
+* Fonction permettant la traduction francais->javanais  par recursivite
+*/
+ function francaisVersJavanaisRegex(texteATraduire){
+	//detecte un succession consonne-voyelle et intercalle alors les lettres 'av'
+	texteATraduire = texteATraduire.replace(/([BCÇDFGHJKLMNPQRSTVWXZbcçdfghjklmnpqrstvwxz' ]{1})([AÀÂEÉÈÊËIÎÏOÔUÛÙaàâeéèêëiîïoôuûù]{1})/g, "$1av$2");
+ 	//detecte une voyelle en debut de phrase et prefixe avec les lettres 'av'
+	texteATraduire = texteATraduire.replace(/^([AÀÂEÉÈÊËIÎÏOÔUÛÙaàâeéèêëiîïoôuûù]{1})/g, "av$1");
+	return texteATraduire;
+
 }
 
 exports.javanaisVersFrancais = function (texteATraduire){
-  return texteATraduire;
+  return javanaisVersFrancaisRegex(texteATraduire);
+}
+
+/*
+* Fonction permettant la traduction  javanais->francais par regexp
+*/
+ function javanaisVersFrancaisRegex(texteATraduire){
+	texteATraduire = texteATraduire.replace(/([BCÇDFGHJKLMNPQRSTVWXZbcçdfghjklmnpqrstvwxz' ]{1})(av|AV)([AÀÂEÉÈÊËIÎÏOÔUÛÙaàâeéèêëiîïoôuûù]{1})/g, "$1$3");
+	texteATraduire = texteATraduire.replace(/^av([AÀÂEÉÈÊËIÎÏOÔUÛÙaàâeéèêëiîïoôuûù]{1})/g, "$1");
+	return texteATraduire;
+}
+
+
+
+/*
+* Fonction lancant la trduction francais vers javanais
+*/
+function traduireEnJavanais(methode) {
+	var txtATraduire = $('textarea#txtAreaFrancais_'+methode).val();
+	if(1==methode){
+		$('textarea#txtAreaJavanais_'+methode).val(francaisVersJavanaisRegex(txtATraduire));
+	}
+}
+
+
+/*
+* Fonction lancant la trduction javanais vers francais
+*/
+function traduireEnFrancais(methode) {
+	var txtATraduire = $('textarea#txtAreaJavanais_'+methode).val();
+	if(1==methode){
+		$('textarea#txtAreaFrancais_'+methode).val(javanaisVersFrancaisRegex(txtATraduire));
+	}else if(2==methode){
+		$('textarea#txtAreaFrancais_'+methode).val(javanaisVersFrancaisRecursive(txtATraduire));
+
+	}
 }
--- test/javanais-main-test.js
+++ test/javanais-main-test.js
@@ -1,9 +1,9 @@
 var expect = require("chai").expect;
 var traducteur = require("../js/javanais-main");
 
-describe("Tradcuteur javanais-francais", function(){
+describe("Tradcuteur francais<->javanais", function(){
 
-	describe("Traducteur via recursive francais->javanais", function(){
+	describe("Traducteur via regexp", function(){
 
 		it("convertit de francais a javanais", function() {
 			var jBonjour   = traducteur.francaisVersJavanais("bonjour");
@@ -11,16 +11,15 @@ describe("Tradcuteur javanais-francais", function(){
 			var jMoyen  = traducteur.francaisVersJavanais("moyen");
 			var jExemple  = traducteur.francaisVersJavanais("exemple");
 			var jAu  = traducteur.francaisVersJavanais("au");
+			var jLarch  = traducteur.francaisVersJavanais("Et maintenant quelque chose de complètement différent, le mélèze");
 
 			expect(jBonjour).to.equal("bavonjavour");
 			expect(jChante).to.equal("chavantave");
 			expect(jMoyen).to.equal("mavoyen");
 			expect(jExemple).to.equal("avexavemplave");
 			expect(jAu).to.equal("avau");
+			expect(jLarch).to.equal("avEt mavaintavenavant qavuelqavue chavosave dave cavomplavètavemavent daviffavéravent, lave mavélavèzave");
 		});
-	});
-
-	describe("Traducteur via regexp javanais->francais", function(){
 
 		it("convertit de javanais a francais", function() {
 			var fBonjour   = traducteur.javanaisVersFrancais("bavonjavour");
@@ -28,13 +27,18 @@ describe("Tradcuteur javanais-francais", function(){
 			var fMoyen  = traducteur.javanaisVersFrancais("mavoyen");
 			var fExemple  = traducteur.javanaisVersFrancais("avexavemplave");
 			var fAu  = traducteur.javanaisVersFrancais("avau");
-
+			var fLarch  = traducteur.javanaisVersFrancais("avEt mavaintavenavant qavuelqavue chavosave dave cavomplavètavemavent daviffavéravent, lave mavélavèzave");
 			expect(fBonjour).to.equal("bonjour");
 			expect(fChante).to.equal("chante");
 			expect(fMoyen).to.equal("moyen");
 			expect(fExemple).to.equal("exemple");
-			expect(fAu).to.equal("au");
+			expect(fLarch).to.equal("Et maintenant quelque chose de complètement différent, le mélèze");
 		});
+	});
+
+	describe("Traducteur via recursive javanais->francais", function(){
+
+
 
 	})
 
